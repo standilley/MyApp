0b4e34e5133c5dd79140c6c10ae597ad3de25d56
standilley <standilley@hotmail.com>
2025-11-05 15:38:40 -0300

    feat(domain): criada a camada de domínio com entidade Produto, regras de negócio e interface de repositório

---
+++ src/Backend/MyApp.Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using MyApp.Domain.Entities;
+
+namespace MyApp.Domain.Interfaces
+{
+    public interface IProdutoRepository
+    {
+        Task<Produto> ObterPorIdAsync(Guid id);
+        Task<IEnumerable<Produto>> ObterTodosAsync();
+        Task AdicionarAsync(Produto produto);
+        Task AtualizarAsync(Produto produto);
+        Task RemoverAsync(Guid id);
+    }
+}
